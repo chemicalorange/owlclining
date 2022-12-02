681f5e86fbb1dd137d01f95c19b78951a6c14276
SergeyLeontyev <46062910+SergeyLeontyev@users.noreply.github.com>
2022-12-02 13:54:38 +0700

    123

--- assets/js/how-block.js
+++ assets/js/how-block.js
@@ -1,31 +1,40 @@
 let how = document.querySelectorAll('.how-info');
+let howAll = [how];
+console.log(howAll)
 
 const toggoleClassTarget = (elem) => {
-		elem.classList.toggle('how-info__btn-active');
-		elem.parentNode.querySelector('.how-info__text').classList.toggle('how-info__text-active');
+	elem.parentNode.classList.toggle('how-info__active');
+	return;
 }
 
-how.forEach((howItem) => {
-		let howBtn = howItem.querySelector('.how-info__btn');
-		let howText = howItem.querySelector('.how-info__text');
-		howItem.addEventListener('click', (event) => {
-				let target = event.target
-				if (target.classList.contains('how-info__btn-active')) {
-						toggoleClassTarget(target)
-						return
-				}
-				how.forEach((howActive) => {
-						howActive.querySelector('.how-info__btn').classList.remove('how-info__btn-active');
-						howActive.querySelector('.how-info__text').classList.remove('how-info__text-active');
-				});
-				toggoleClassTarget(target)
-
+document.addEventListener( 'click', (e) => {
+	const withinBoundaries = e.composedPath().includes(howAll);
+	if ( ! withinBoundaries ) {
+		how.forEach((howActive) => {
+			howActive.classList.remove('how-info__active');
 		});
+	}
+})
+
+how.forEach((howItem) => {
+	howItem.addEventListener('click', (event) => {
+		let target = event.target;
+		if(target.parentNode.classList.contains('how-info__active')){
+			target.parentNode.classList.remove('how-info__active')
+		}
+		target.parentNode.classList.toggle('how-info__active');
+		how.forEach((howActive) => {
+			howActive.classList.remove('how-info__active');
+		});
+		console.log(target.parentNode)
+		toggoleClassTarget(target);
+		event.stopPropagation();
+		// return;
+	});
+
 
 });
 
-
-//
 
 let howBox = document.querySelectorAll('.how-card');
 var howTabs = document.querySelector('.how-tabs');
@@ -33,7 +42,7 @@ let howTab = document.querySelectorAll('.how-tabs__item');
 let i = 1;
 
 
-if(howTab.length === 0){
+if (howTab.length === 0) {
 	howTabs.style.display = "none";
 } else {
 	howTab[0].classList.add('how-tabs__item-active');
